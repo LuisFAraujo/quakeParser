203203533a7ea84c5bcfafcdebe1839da5ec8f04
LuisFAraujo <luisfelipe.araujo.contato@gmail.com>
2021-06-17 18:33:18 -0300

    Criação da classe que vai salvar os dados do Jogador(Player)

--- src/est/estagio/models/Player.java
+++ src/est/estagio/models/Player.java
@@ -1,5 +1,31 @@
 package est.estagio.models;
 
 public class Player {
+    private String name;
+    private int kills;
 
+    public Player(String name) {
+        this.name = name;
+        this.kills = 0;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public int getKills() {
+
+        return kills;
+    }
+
+    public void setKills(int kills) {
+
+        this.kills = kills;
+    }
 }
